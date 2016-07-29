c08f1baba05114ec7e7f4dac56b2d4a7fe5db49b
noidhoon <noidhoon@gmail.com>
2016-07-29 11:16:01 +0200

    0.0.1

---
+++ exp.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var express = require('./exp/express');
+module.exports.express = express;
+
+var mongoose = require('./exp/mongoose');
+module.exports.mongoose = mongoose;
+
+var users = require('./exp/models/users');
+module.exports.users = users;
+
+var install = function( application )
+{
+    users.count({}, function( error, count )
+    {
+        if( error ) throw error;
+        if( !count )
+        {
+            var user = new users({ username: application.username, password: application.password });
+            user.save( function( errors, user )
+            {
+                if( errors ) throw errors;
+                console.log('User creato: ' + user.username + ' ' + user.password );
+            });
+        }
+    });
+}
+
+module.exports.install = install;
