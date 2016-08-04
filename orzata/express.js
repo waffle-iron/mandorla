535858ff04b168011710ff0a08b7305a5513b8fa
noidhoon <noidhoon@gmail.com>
2016-08-04 20:30:59 +0200

    0.0.2

--- orzata/express.js
+++ orzata/express.js
@@ -4,8 +4,7 @@ var express = require('express');
 var application = express();
 
 var bodyparser = require('body-parser');
-
-application.use( bodyparser.json() );
 application.use( bodyparser.urlencoded({ extended: true }));
+application.use( bodyparser.json() );
 
 module.exports = application;
