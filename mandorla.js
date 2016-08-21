7e7806551fb60e6777ed29278573d389ad7b6626
noidhoon <noidhoon@gmail.com>
2016-08-21 18:12:22 +0200

    0.1

---
+++ mandorla.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var express = require('./mandorla/configurations/express');
+module.exports.express = express;
+
+var mongoose = require('./mandorla/configurations/mongoose');
+module.exports.mongoose = mongoose;
