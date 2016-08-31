fc7e13cc9ae42266d8097af81db32b0f175c884a
noidhoon <noidhoon@gmail.com>
2016-08-31 12:40:03 +0200

    moved mandorla/node_configurations to ./node_configurations

--- mandorla.js
+++ mandorla.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var mandorla = {};
-mandorla.nconf = require('./mandorla/node_configurations/nconf');
-mandorla.mongoose = require('./mandorla/node_configurations/mongoose')( mandorla.nconf );
-mandorla.application = require('./mandorla/node_configurations/express')( mandorla.nconf );
+mandorla.nconf = require('./node_configurations/nconf');
+mandorla.mongoose = require('./node_configurations/mongoose')( mandorla.nconf );
+mandorla.application = require('./node_configurations/express')( mandorla.nconf );
 
 module.exports = mandorla;
